0da0e611069fafc31cc52bd21d91e542a847a410
Jordan Mackie <jordan@gravitysketch.com>
2023-10-25 18:08:34 -0400

    remove focused element on unfocus

--- client/src/contentscripts/popup/App.tsx
+++ client/src/contentscripts/popup/App.tsx
@@ -1,7 +1,30 @@
-import useFocusedInputElement from "./hooks/useFocusedInputElement.ts";
+import useFocusedInputElement, {InputElement} from "./hooks/useFocusedInputElement.ts";
+import {useState} from "preact/hooks";
+import {useEffect} from "preact/compat";
+import {Container, ContainerProps, MantineProvider, Text} from "@mantine/core";
+
+
+const useContainerProps = (focusedElement: InputElement | undefined): ContainerProps => {
+    const rect = focusedElement?.getBoundingClientRect()
+    console.log({rect})
+    return {
+        style: {
+            position: "absolute",
+            bottom: rect?.top,
+            left: rect?.left,
+        },
+        w: "20rem",
+        h: "50rem",
+    }
+}
 
 const App = () => {
+    const [showPopup, setShowPopup] = useState(false);
     const focusedElement = useFocusedInputElement();
+    const containerProps = useContainerProps(focusedElement)
+
+    console.log({focusedElement})
+
     // TODO render "submit question", fire the below to populate the field
     // const res = await sendMessageToBackgroundScript({type: MessageType.focusEvent, inputElement: activeElement})
     // if (res?.type !== MessageType.questionAnswered) {
@@ -11,7 +34,17 @@ const App = () => {
     // // @ts-ignore
     // activeElement.value = res.answer
 
-    return focusedElement ? <div>hello!</div> : null
+    useEffect(() => {
+        setShowPopup(true);
+    }, [focusedElement])
+
+    if (!(focusedElement && showPopup)) return null;
+    else return <MantineProvider>
+        <Container {...containerProps}>
+            <Text>hello! Click <a onClick={() => setShowPopup(false)}>here</a> to close this
+            </Text>
+        </Container>
+    </MantineProvider>
 }
 
 export default App;
--- client/src/contentscripts/popup/hooks/useFocusedInputElement.ts
+++ client/src/contentscripts/popup/hooks/useFocusedInputElement.ts
@@ -2,14 +2,23 @@ import {useEffect} from "preact/compat";
 import createFocusInputHandler from "./createFocusInputHandler.tsx";
 import {useState} from "preact/hooks";
 
+export type InputElement = HTMLInputElement | HTMLTextAreaElement
+
 const useFocusedInputElement = () => {
-    const [element, setElement] = useState<HTMLInputElement | HTMLTextAreaElement>();
+    const [element, setElement] = useState<InputElement>();
 
     useEffect(() => {
-        const handler = createFocusInputHandler(setElement)
         // for focus, we fire during the capture phase as focus doesn't bubble
-        document.addEventListener('focus', handler, true)
-        return () => document.removeEventListener("focus", handler, true)
+        const focusHandler = createFocusInputHandler(setElement)
+        document.addEventListener('focus', focusHandler, true)
+        // on focusout, we remove the currently focused element
+        const focusOutHandler = () => setElement(undefined);
+        document.addEventListener('focusout', focusOutHandler)
+
+        return () => {
+            document.removeEventListener('focus', focusHandler, true)
+            document.removeEventListener('focusout', focusOutHandler)
+        }
     }, [])
 
     return element;
